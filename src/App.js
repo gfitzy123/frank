53e2ee6cad890d745456acfe2713f9ddc4bf2e33
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-11 14:08:12 -0600

    Added common components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ paddingTop: 65 }}>Hello world</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
