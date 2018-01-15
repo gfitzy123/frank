7c27a7de455a45bdef8758f55ae81a56afc019b9
natasha-robarge <robarge.natasha7@gmail.com>
2018-01-15 15:42:31 -0600

    Testing victory-native

--- src/components/UsageChart.js
+++ src/components/UsageChart.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import Data from '../data.json';
+import Svg from "react-native-svg";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryScatter,
+  VictoryLegend
+} from 'victory-native';
+import { VictoryTheme } from "victory-core";
 
 class UsageChart extends Component {
   constructor(props) {
@@ -11,22 +19,34 @@ class UsageChart extends Component {
     }
   }
 
-  check() {
-    for(let i = 0; i < Data.length; i++) {
-      this.setState({
-        text: Data[0].TotalNewCharges
-      })
-    }
-  }
-
-  //Loop through json data
-  //Display data from other component with chart
-  //import component to file.
 
   render() {
     return (
       <View>
-        <Text style={{ color: '#fff' }}>Chart here, yo {this.state.text}</Text>
+        <VictoryChart polar theme={VictoryTheme.material}>
+          <VictoryBar
+            style={{ data: { fill: "tomato", opacity: 0.5 } }}
+            data={[
+              { x: 15, y: 20, label: 1, fill: "red" },
+              { x: 25, y: 30, label: 2, fill: "orange" },
+              { x: 35, y: 65, label: 3, fill: "gold" },
+              { x: 40, y: 50, label: 4, fill: "blue" },
+              { x: 45, y: 40, label: 5, fill: "cyan" },
+              { x: 50, y: 30, label: 6, fill: "green" }
+            ]}
+          />
+          <VictoryScatter
+            style={{ data: { fill: "black" } }}
+            data={[
+              { x: 15, y: 20 },
+              { x: 25, y: 30 },
+              { x: 35, y: 65 },
+              { x: 40, y: 50 },
+              { x: 45, y: 40 },
+              { x: 50, y: 30 }
+            ]}
+          />
+        </VictoryChart>
       </View>
     );
   }
