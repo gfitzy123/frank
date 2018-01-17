088cce673b64d40b28a5fdbaed3dc3b264e26868
natasha-robarge <robarge.natasha7@gmail.com>
2018-01-17 14:55:58 -0600

    Got map marker on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,11 +7,21 @@ class Map extends Component {
     return (
       <View>
         <MapHeader headerText="Find Meters" />
+        <Image style={styles.mapMarkerStyle} source={require('../img/map-mark.png')} />
         <Image
           source={require('../img/map-img.png')}
         />
       </View>
     );
+  }
+}
+
+const styles = {
+  mapMarkerStyle: {
+    // elevation: 2,
+    top: 100,
+    left: 80,
+    position: 'absolute',
   }
 }
 
