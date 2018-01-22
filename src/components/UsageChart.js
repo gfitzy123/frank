9a3d98decece46015ef7c5a46a2f5e35e9c3290d
natasha-robarge <robarge.natasha7@gmail.com>
2018-01-22 12:23:34 -0600

    Fixed chart styling issues finally

--- src/components/UsageChart.js
+++ src/components/UsageChart.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import Data from '../data.json';
 import Svg from "react-native-svg";
 import {
-  VictoryLine,
+  VictoryBar,
   VictoryChart,
+  VictoryLine,
   VictoryAxis
 } from 'victory-native';
 import { VictoryTheme } from "victory-core";
@@ -59,53 +60,83 @@ class UsageChart extends Component {
   }
 
   render() {
-    return (
-      <View>
-        {/* x = EndDate y = Consumption */}
-        <VictoryChart
-          domain={[0, 1]}
-          style={{ domain: { fontSize: 10, fill: '#fff' } }}
-          height={140}
-          width={350}
-        >
-          <VictoryLine
-            style={{
-              data: { stroke: "#02b3a4" },
-            }}
+    var data = [
+      { x: this._getFirstLetterOfMonth("08/19/2014"), y: 414},
+      { x: this._getFirstLetterOfMonth("10/18/2014"), y: 333 },
+      { x: this._getFirstLetterOfMonth("09/18/2014"), y: 348 },
+      { x: this._getFirstLetterOfMonth("07/17/2014"), y: 327 },
+      { x: this._getFirstLetterOfMonth("06/18/2014"), y: 380 },
+      { x: this._getFirstLetterOfMonth("05/20/2014"), y: 419 },
+      { x: this._getFirstLetterOfMonth("01/18/2014"), y: 399 },
+      { x: this._getFirstLetterOfMonth("04/17/2014"), y: 316 },
+      { x: this._getFirstLetterOfMonth("03/20/2014"), y: 345 },
+      { x: this._getFirstLetterOfMonth("11/16/2013"), y: 400 },
+      { x: this._getFirstLetterOfMonth("07/18/2015"), y: 335 },
+      { x: this._getFirstLetterOfMonth("12/16/2015"), y: 311 },
+      { x: this._getFirstLetterOfMonth("11/18/2015"), y: 422 },
+      { x: this._getFirstLetterOfMonth("01/19/2016"), y: 422 },
+      { x: this._getFirstLetterOfMonth("02/17/2016"), y: 376 },
+      { x: this._getFirstLetterOfMonth("03/18/2016"), y: 325 },
+      { x: this._getFirstLetterOfMonth("10/16/2013"), y: 307 },
+      { x: this._getFirstLetterOfMonth("01/25/2016"), y: 350 },
+      { x: this._getFirstLetterOfMonth("11/24/2015"), y: 350 },
+      { x: this._getFirstLetterOfMonth("12/24/2015"), y: 350 },
+      { x: this._getFirstLetterOfMonth("02/23/2016"), y: 350 },
+      { x: this._getFirstLetterOfMonth("03/24/2016"), y: 350 },
+      { x: this._getFirstLetterOfMonth("04/22/2016"), y: 350 },
+    ]
 
-            data={[
-              { x: this._getFirstLetterOfMonth("08/19/2014"), y: 414},
-              { x: this._getFirstLetterOfMonth("10/18/2014"), y: 333 },
-              { x: this._getFirstLetterOfMonth("09/18/2014"), y: 348 },
-              { x: this._getFirstLetterOfMonth("07/17/2014"), y: 327 },
-              { x: this._getFirstLetterOfMonth("06/18/2014"), y: 380 },
-              { x: this._getFirstLetterOfMonth("05/20/2014"), y: 419 },
-              { x: this._getFirstLetterOfMonth("01/18/2014"), y: 399 },
-              { x: this._getFirstLetterOfMonth("04/17/2014"), y: 316 },
-              { x: this._getFirstLetterOfMonth("03/20/2014"), y: 345 },
-              { x: this._getFirstLetterOfMonth("11/16/2013"), y: 400 },
-              { x: this._getFirstLetterOfMonth("07/18/2015"), y: 335 },
-              { x: this._getFirstLetterOfMonth("12/16/2015"), y: 311 },
-              { x: this._getFirstLetterOfMonth("11/18/2015"), y: 422 },
-              { x: this._getFirstLetterOfMonth("01/19/2016"), y: 422 },
-              { x: this._getFirstLetterOfMonth("02/17/2016"), y: 376 },
-              { x: this._getFirstLetterOfMonth("03/18/2016"), y: 325 },
-              { x: this._getFirstLetterOfMonth("10/16/2013"), y: 307 },
-              { x: this._getFirstLetterOfMonth("01/25/2016"), y: 350 },
-              { x: this._getFirstLetterOfMonth("11/24/2015"), y: 350 },
-              { x: this._getFirstLetterOfMonth("12/24/2015"), y: 350 },
-              { x: this._getFirstLetterOfMonth("02/23/2016"), y: 350 },
-              { x: this._getFirstLetterOfMonth("03/24/2016"), y: 350 },
-              { x: this._getFirstLetterOfMonth("04/22/2016"), y: 350 },
-              { x: this._getFirstLetterOfMonth("01/04/2016"), y: 1166416.8 },
-              { x: this._getFirstLetterOfMonth("12/02/2015"), y: 1974689.1 },
-              { x: this._getFirstLetterOfMonth("02/03/2016"), y: 1767388.8 },
-              { x: this._getFirstLetterOfMonth("03/07/2016"), y: 1962416 },
-              { x: this._getFirstLetterOfMonth("04/01/2016"), y: 1507978.8 },
-            ]}
-            // interpolation="cardinal"
-          />
-        </VictoryChart>
+    // console.log(data)
+
+    return (
+      <View style={{ height: 135}}>
+        {/* x = EndDate y = Consumption */}
+        <Svg
+          style={{ flex: 1, marginTop: 0, paddingTop: 0 }}
+          height={200}
+          width={300}
+          viewBox={"0 0 140 350"}
+          preserveAspectRatio="none"
+          >
+          <VictoryChart
+            domainPadding={15}
+            style={{
+              ticks: { fill: '#fff' },
+              tickLabels: { fill: '#fff' },
+            }}
+            height={175}
+            width={385}
+          >
+            <VictoryAxis
+              style={{
+                axis: { stroke: "#02b3a4" },
+                ticks: { fill: "#fff" },
+                tickLabels: { fill: '#fff' }
+              }}
+              // label="Month"
+              fixLabelOverlap={true}
+            />
+            <VictoryAxis
+              style={{
+                axis: { stroke: "#02b3a4" },
+                ticks: { fill: "#fff" },
+                tickLabels: { fill: '#fff' }
+              }}
+              tickValues={[0]}
+              tickFormat={(ticks) => ticks + ' kWh' }
+              dependentAxis
+              // label="kWh"
+              fixLabelOverlap={true}
+            />
+
+            <VictoryBar
+              style={{
+                data: { stroke: "#02b3a4", fill: "#000" }
+              }}
+              data={data}
+            />
+          </VictoryChart>
+        </Svg>
       </View>
     );
   }
