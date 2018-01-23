5e1614f507a0173b2405f9baa927d6c4ea930ad2
natasha-robarge <robarge.natasha7@gmail.com>
2018-01-23 12:27:11 -0600

    Added new background for home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, Text, Image, ImageBackground } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { CardSection, Button } from './common/Index';
 
@@ -7,32 +7,54 @@ class Home extends Component {
   render() {
     return (
       <View style={styles.viewStyle}>
-        <View style={styles.imgContainerStyle}>
-          <Image style={styles.imgStyle} source={require('../img/logo.png')} accessbilityLabel="Frank's logo" />
-        </View>
-        <View style={{ paddingTop: 200 }}>
-          <Button onPress={() => Actions.auth()}>Sign In</Button>
-        </View>
+        <Image
+          style={{
+            flex: 1,
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+          }}
+          source={{ uri: 'https://lh3.googleusercontent.com/-rAN3iNbA-g8/WmZDJwbUMRI/AAAAAAAABC8/dvx6BPX0cMEH_SQkN9HAdqvWkSuJn5IvQCL0BGAYYCw/h321/2018-01-22.gif' }}
+          >
+          <View style={styles.imgContainerStyle}>
+            <Image style={styles.imgStyle} source={require('../img/logo.png')} accessbilityLabel="Frank's logo" />
+          </View>
+          <Text style={styles.sloganStyle}>FRANK SOLUTIONS FOR COMMERCIAL ENERGY</Text>
+          <View style={{ paddingTop: 100 }}>
+            <Button onPress={() => Actions.auth()}>Sign In</Button>
+          </View>
+        </Image>
       </View>
     );
   }
 }
 
+
 const styles = {
   viewStyle: {
-    backgroundColor: '#123c66',
-    flex: 1
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
   },
   imgContainerStyle: {
     justifyContent: 'center',
     alignItems: 'center',
     marginLeft: 10,
-    marginRight: 10,
-    marginTop: 100
+    marginRight: 10
   },
   imgStyle: {
     width: 100,
-    height: 100
+    height: 100,
+  },
+  sloganStyle: {
+    marginTop: 150,
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: '800',
+    backgroundColor: 'transparent'
   }
 }
 
