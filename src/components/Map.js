cc042cbbc64d9418ad78d4940d22b61987be0135
natasha-robarge <robarge.natasha7@gmail.com>
2018-02-27 21:41:32 -0600

    Got mapbox to display on map screen

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,17 +8,26 @@ import {
 } from 'react-native';
 import { MapHeader } from './common/Index';
 import NavBar from './NavBar';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+
+Mapbox.setAccessToken('pk.eyJ1Ijoic2V2ZW52YXJpYW5jZSIsImEiOiJjamI4OTAydTEwMWV5MnFxZ2hidTBvY3VmIn0.3A6cLnUZ5gbPUJKfLbbmIA');
 
 class Map extends Component {
   render() {
     return (
-      <View>
-        <MapHeader headerText="Find Meters" />
-        <Image
+      <View style={{ flex: 1 }}>
+        {/* <MapHeader headerText="Find Meters" /> */}
+        {/* <Image
           style={{ width: 450, height: 615, marginBottom: 50 }}
           source={require('../img/MapHolder.png')}
+        /> */}
+        <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Street}
+            zoomLevel={15}
+            centerCoordinate={[11.256, 43.770]}
+            style={{ flex: 1 }}
         />
-        <NavBar />
+        {/* <NavBar /> */}
       </View>
     );
   }
